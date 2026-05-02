6ea857bade714dfcac4e956dbdc3fde48c6dee1e
Dylan Anthony <dmatheson23@gmail.com>
2026-05-02 12:36:14 +0200

    feat: update cart page

---
+++ pages/CartPage.ts
@@ -0,0 +1,22 @@
+import { Page, Locator } from '@playwright/test';
+
+export class CartPage {
+  readonly page: Page;
+  readonly cartItems: Locator;
+  readonly checkoutButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+
+    this.cartItems = page.locator('.cart_item');
+    this.checkoutButton = page.locator('#checkout');
+  }
+
+  async getCartItemCount() {
+    return await this.cartItems.count();
+  }
+
+  async proceedToCheckout() {
+    await this.checkoutButton.click();
+  }
+}
